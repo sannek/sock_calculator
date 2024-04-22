b3c172b297af7de759525f8ff469dcf27e01cd1b
Sanne Kalkman <sannekalkman@gmail.com>
2024-04-22 21:13:00 +0200

    add prod build script

---
+++ bin/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+gleam run -m lustre/dev build app
+
+mkdir -p prod/priv/static
+
+cp ./build/dev/javascript/lustre_ui/priv/static/lustre-ui.css prod/priv/static/lustre-ui.css
+
+cp ./priv/static/sock_calculator.mjs prod/priv/static/sock_calculator.mjs
+
+cp index.html prod/index.html
+
+sed -i '' 's/\.\/build\/dev\/javascript\/lustre_ui/\./g' prod/index.html
