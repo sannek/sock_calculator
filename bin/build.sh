7c9fcaca36e459cb921cde4d84376b55f7c699d9
Sanne Kalkman <sannekalkman@gmail.com>
2024-04-23 18:00:48 +0200

    rename prod to dist

--- bin/build.sh
+++ bin/build.sh
@@ -2,13 +2,13 @@
 
 gleam run -m lustre/dev build app
 
-mkdir -p prod/priv/static
+mkdir -p dist/priv/static
 
-cp ./build/dev/javascript/lustre_ui/priv/static/lustre-ui.css prod/priv/static/lustre-ui.css
+cp ./build/dev/javascript/lustre_ui/priv/static/lustre-ui.css dist/priv/static/lustre-ui.css
 
-cp ./priv/static/sock_calculator.mjs prod/priv/static/sock_calculator.mjs
-cp ./priv/static/index.css prod/priv/static/index.css
+cp ./priv/static/sock_calculator.mjs dist/priv/static/sock_calculator.mjs
+cp ./priv/static/index.css dist/priv/static/index.css
 
-cp index.html prod/index.html
+cp index.html dist/index.html
 
-sed -i '' 's/\.\/build\/dev\/javascript\/lustre_ui/\./g' prod/index.html
+sed -i '' 's/\.\/build\/dev\/javascript\/lustre_ui/\./g' dist/index.html
